2ce9c6a4ecef356ecfbdb90550cf3eb764dd5bcb
rkarmadhn <rakaaputtra1@gmail.com>
2023-01-18 20:42:50 +0700

    More Fixed

--- js/exclusive.js
+++ js/exclusive.js
@@ -30,7 +30,6 @@ const loadUserData = (data) => {
                 <p class="p-content m-0">${el.rating}</p>
               </div>
             </div>
-            <br/>
             <div class="d-flex justify-content-between align-content-center my-2">
               <div class="d-flex justify-content-center align-items-center gap-2">
                 <i class="fa-solid fa-location-dot" style="color: #666"></i>
